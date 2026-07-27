c3dee4638ea966eb3e41c1bb01c83b2b83826b71
vivianacostas1 <vialcore123@hotmail.com>
2026-07-27 06:23:19 -0400

    fix auth login service

--- src/api/auth.service.ts
+++ src/api/auth.service.ts
@@ -1,9 +1,16 @@
 import { api } from './axios';
-import type { AuthResponse } from '../types'; // <-- Agrega 'type' aquí
+import type { AuthResponse } from '../types';
 
 export const authService = {
   async login(email: string, password: string): Promise<AuthResponse> {
-    const response = await api.post<AuthResponse>('/usuarios/login', { email, password });
+    const response = await api.post<AuthResponse>(
+      '/usuarios/login',
+      {
+        email,
+        password,
+      }
+    );
+
     return response.data;
   },
 };
